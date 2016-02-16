583cc9e700d460344726e5f0d8cd9275b3dcaa25
mogh <mogh@example.com>
2016-02-16 09:40:54 -0800

    added articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,9 @@
 class Article < ActiveRecord::Base
-	has_many :categories
+	belongs_to :category
+	
+	validates_presence_of :title
+  	validates_presence_of :content
 
-	scope :active, where('active = ?', true)
+  	scope :active, where('active = ?', true)
   	scope :alphabetical, order('title')
-
-
- 	validates_presence_of :title
- 	validates_presence_of :content
 end
